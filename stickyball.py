ae288295e213cddab5c580342c743c2bda74a0c2
Lius <142863069+noturnachs@users.noreply.github.com>
2023-12-01 10:48:02 +0800

    Update stickyball.py

--- stickyball.py
+++ stickyball.py
@@ -1,8 +1,5 @@
 from ball import Ball
 
-# import random
-
-
 class StickyBall(Ball):
     def __init__(
         self,
@@ -23,10 +20,8 @@ class StickyBall(Ball):
 
     def handle_release(self, mouse_pos):
         if self.dragging and (self.velocity[0] ** 2 + self.velocity[1] ** 2 < 1):
-            # Add current ball position to drag_line list
             self.drag_line.append(self.current_pos)
 
-            # Calculate the distance and direction of the mouse drag
             drag_distance = (
                 (self.current_pos[0] - mouse_pos[0]) ** 2
                 + (self.current_pos[1] - mouse_pos[1]) ** 2
@@ -37,11 +32,8 @@ class StickyBall(Ball):
                 self.current_pos[1] - mouse_pos[1],
             ]
 
-            # Check if drag_distance is not zero
             if drag_distance != 0:
-                # Normalize the direction vector
                 drag_direction = [i / drag_distance for i in drag_direction]
-                # Make the velocity proportional to the drag distance and in the direction of the drag
                 self.velocity = [
                     i * drag_distance * 0.05 * self.speed_multiplier
                     for i in drag_direction
@@ -52,8 +44,6 @@ class StickyBall(Ball):
             self.dragging = False
 
     def handle_collision(self, screen_width, screen_height, obstacles):
-        # Check collision with obstacles
-        # Check collision with obstacles
         for obstacle in obstacles:
             if (
                 self.current_pos[0] + self.radius >= obstacle.x - self.radius
@@ -63,8 +53,8 @@ class StickyBall(Ball):
                 and self.current_pos[1] - self.radius
                 <= obstacle.y + obstacle.height + self.radius
             ):
-                self.velocity = [0, 0]  # Stop movement
-                break  # Exit the loop after the first collision
+                self.velocity = [0, 0]
+                break 
 
     def set_speed_multiplier(self, multiplier: int):
         self.speed_multiplier = multiplier
